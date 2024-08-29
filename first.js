836568d10711308ab7f4d596928ae2fd423b0555
Chetankumar <chetankumar7602@gmail.com>
2024-08-29 17:31:15 +0530

    Modified JS files

--- first.js
+++ first.js
@@ -23,16 +23,15 @@ const checkWinner = () => {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
-        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
-            if (pos1Val === pos2Val && pos2Val === pos3Val) {
-                alert(`${pos1Val} wins!`);
-                updateScore(pos1Val);
-                drawLine(pattern);
-                disableAllBoxes();
-                return;
-            }
+
+        if (pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+            alert(`${pos1Val} wins!`);
+            updateScore(pos1Val);
+            disableAllBoxes();
+            return;
         }
     }
+
     if ([...boxes].every(box => box.innerText !== "")) {
         alert("It's a draw!");
     }
@@ -54,46 +53,27 @@ const disableAllBoxes = () => {
     });
 };
 
-const drawLine = (pattern) => {
-    const box1 = boxes[pattern[0]];
-    const box2 = boxes[pattern[2]];
-    const line = document.createElement("div");
-    line.classList.add("line");
-
-    const rect1 = box1.getBoundingClientRect();
-    const rect2 = box2.getBoundingClientRect();
-
-    line.style.left = `${(rect1.left + rect1.right) / 2}px`;
-    line.style.top = `${(rect1.top + rect1.bottom) / 2}px`;
-    line.style.width = `${Math.hypot(rect2.left - rect1.left, rect2.top - rect1.top)}px`;
-    line.style.transform = `rotate(${Math.atan2(rect2.top - rect1.top, rect2.left - rect1.left) * 180 / Math.PI}deg)`;
-
-    document.querySelector(".game").appendChild(line);
-};
-
 const resetGame = () => {
     boxes.forEach(box => {
         box.innerText = "";
         box.disabled = false;
     });
     turnO = true;
-    const line = document.querySelector(".line");
-    if (line) {
-        line.remove();
-    }
 };
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        if (turnO) {
-            box.innerText = "O";
-            turnO = false;
-        } else {
-            box.innerText = "X";
-            turnO = true;
+        if (box.innerText === "") {
+            if (turnO) {
+                box.innerText = "O";
+                turnO = false;
+            } else {
+                box.innerText = "X";
+                turnO = true;
+            }
+            box.disabled = true;
+            checkWinner();
         }
-        box.disabled = true;
-        checkWinner();
     });
 });
 
